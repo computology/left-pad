8fa0f9dfa10a273a2fc63ce1925d53a8d43f40b9
capotej <jcapote@gmail.com>
2018-02-26 16:11:08 -0500

    add debugging statements

--- index.js
+++ index.js
@@ -22,30 +22,49 @@ var cache = [
 function leftPad (str, len, ch) {
   // convert `str` to a `string`
   str = str + '';
+
   // `len` is the `pad`'s length now
   len = len - str.length;
+
+  console.log('DEBUG: str: ' + str + ', len: ' + len, ', ch: ' + ch);
+
   // doesn't need to pad
-  if (len <= 0) return str;
+  if (len <= 0) {
+    return str;
+  }
+
   // `ch` defaults to `' '`
-  if (!ch && ch !== 0) ch = ' ';
+  if (!ch && ch !== 0){
+    ch = ' ';
+  }
+
   // convert `ch` to a `string` cuz it could be a number
   ch = ch + '';
   // cache common use cases
-  if (ch === ' ' && len < 10) return cache[len] + str;
+  if (ch === ' ' && len < 10){
+    return cache[len] + str;
+  }
+
   // `pad` starts with an empty string
   var pad = '';
   // loop
   while (true) {
     // add `ch` to `pad` if `len` is odd
-    if (len & 1) pad += ch;
+    if (len & 1) {
+      pad += ch;
+    }
     // divide `len` by 2, ditch the remainder
     len >>= 1;
     // "double" the `ch` so this operation count grows logarithmically on `len`
     // each time `ch` is "doubled", the `len` would need to be "doubled" too
     // similar to finding a value in binary search tree, hence O(log(n))
-    if (len) ch += ch;
+    console.log('DEBUG: (loop) str: ' + str + ', len: ' + len, ', ch: ' + ch);
+    if (len) {
+      ch += ch;
+    } else {
     // `len` is 0, exit the loop
-    else break;
+      break;
+    }
   }
   // pad `str`!
   return pad + str;
